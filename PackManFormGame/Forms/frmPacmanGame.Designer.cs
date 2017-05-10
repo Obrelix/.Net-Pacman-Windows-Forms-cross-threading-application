39928a0ed3f8734e9d431d7656f75e99a0c9e439
Ioannis Alimpertis <ialimpertis@outlook.com>
2017-05-11 02:28:38 +0300

    Lists added to a new Class

--- PackManFormGame/Forms/frmPacmanGame.Designer.cs
+++ PackManFormGame/Forms/frmPacmanGame.Designer.cs
@@ -59,11 +59,11 @@
             // lblPosition
             // 
             this.lblPosition.AutoSize = true;
-            this.lblPosition.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.lblPosition.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.lblPosition.ForeColor = System.Drawing.Color.White;
             this.lblPosition.Location = new System.Drawing.Point(12, 46);
             this.lblPosition.Name = "lblPosition";
-            this.lblPosition.Size = new System.Drawing.Size(14, 13);
+            this.lblPosition.Size = new System.Drawing.Size(10, 9);
             this.lblPosition.TabIndex = 1;
             this.lblPosition.Text = "0";
             // 
